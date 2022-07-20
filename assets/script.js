289e227ffa5964ca5b27168722bb565082b811b3
Jerry <jerry.lucas@binus.ac.id>
2022-07-21 00:19:17 +0700

    feat: udpate our program section

---
+++ assets/script.js
@@ -0,0 +1,134 @@
+function clicked1() {
+    document.querySelector("#satu").src="assets/images/birthday-party.jpg";
+    document.querySelector("#dua").src="assets/images/LDK.jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/images/competition.jpg";
+    document.querySelector("#enam").src="assets/images/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/images/cil_birthday-cake.jpg";
+    document.querySelector("#boxh3").innerHTML="Birthday Party";
+    document.querySelector("#boxp").innerHTML="Bagi kalian yang merasa ingin menambah pengetahuan atau mungkin merasa kurang paham dalam memahami materi saat berkuliah. Kalian tenang saja karena BSLC membuka kesempatan bagi mahasiswa Binusian semester 1/2 untuk bergabung sebagai mentee. Nantinya kalian sebagai mentee akan diajar oleh mentor yang merupakan kakak kelas kalian. Jadi tunggu apalagi, mari daftarkan diri kalian menjadi mentee BSLC.";
+}
+
+function clicked2() {
+    document.querySelector("#satu").src="assets/images/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/images/LDK(1).jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/images/competition.jpg";
+    document.querySelector("#enam").src="assets/images/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/images/ldkcek.jpg";
+    document.querySelector("#boxh3").innerHTML="LDK CP";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+}
+
+function clicked3() {
+    document.querySelector("#satu").src="assets/images/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/images/LDK.jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party(1).jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/images/competition.jpg";
+    document.querySelector("#enam").src="assets/images/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/images/birthdayIcon.jpg";
+    document.querySelector("#boxh3").innerHTML="Farewell Party";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+}
+
+function clicked4() {
+    document.querySelector("#satu").src="assets/images/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/images/LDK.jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class(1).jpg";
+    document.querySelector("#lima").src="assets/images/competition.jpg";
+    document.querySelector("#enam").src="assets/images/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/images/la_chalkboard-teacher.jpg";
+    document.querySelector("#boxh3").innerHTML="BSLC Class";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+}
+
+function clicked5() {
+    document.querySelector("#satu").src="assets/images/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/images/LDK.jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/images/competition(1).jpg";
+    document.querySelector("#enam").src="assets/images/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/images/iconoir_leaderboard-star.jpg";
+    document.querySelector("#boxh3").innerHTML="BSLC Competition";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+}
+
+function clicked6() {
+    document.querySelector("#satu").src="assets/images/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/images/LDK.jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/images/competition.jpg";
+    document.querySelector("#enam").src="assets/images/responsi(1).jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/images/book_FILL0_wght400_GRAD0_opsz48 1.jpg";
+    document.querySelector("#boxh3").innerHTML="Responsi";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+}
+
+function clicked7() {
+    document.querySelector("#satu").src="assets/images/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/images/LDK.jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/images/competition.jpg";
+    document.querySelector("#enam").src="assets/images/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar(1).jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/images/school_FILL0_wght400_GRAD0_opsz48 1.jpg";
+    document.querySelector("#boxh3").innerHTML="BSLC Mengajar";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+}
+
+function clicked8() {
+    document.querySelector("#satu").src="assets/images/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/images/LDK.jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/images/competition.jpg";
+    document.querySelector("#enam").src="assets/images/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya(1).jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/images/icon-park-outline_tree.jpg";
+    document.querySelector("#boxh3").innerHTML="Outing Nindya";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+}
+
+function clicked9() {
+    document.querySelector("#satu").src="assets/images/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/images/LDK.jpg";
+    document.querySelector("#tiga").src="assets/images/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/images/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/images/competition.jpg";
+    document.querySelector("#enam").src="assets/images/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/images/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/images/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/images/welcoming party(1).jpg";
+    document.querySelector("#boxicon").src="assets/images/tabler_ballon.jpg";
+    document.querySelector("#boxh3").innerHTML="Welcoming Party";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+}
