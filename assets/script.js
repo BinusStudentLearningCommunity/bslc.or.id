3f89848ae77b9f0e7edafffda5dff1b4de179984
Jerry <jerry.lucas@binus.ac.id>
2022-07-21 01:11:12 +0700

    chore: change content our program

--- assets/script.js
+++ assets/script.js
@@ -70,7 +70,7 @@ function clicked5() {
     document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
     document.querySelector("#boxicon").src="assets/images/iconoir_leaderboard-star.jpg";
     document.querySelector("#boxh3").innerHTML="BSLC Competition";
-    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+    document.querySelector("#boxp").innerHTML="BSLC Competition adalah event yang bekerjasama dengan pihak sponsor untuk menciptakan kompetisi yang menarik bagi seluruh mahasiswa/i Indonesia. Kegiatan ini terdiri dari beberapa tahap penyisihan untuk menuju ke tahap final. BSLC Competition diharapkan mampu menjadi event yang menambah wawasan dan pengalaman mahasiswa/i dengan mengikuti kompetisi yang diadakan pada tahun tersebut.";
 }
 
 function clicked6() {
@@ -85,7 +85,7 @@ function clicked6() {
     document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
     document.querySelector("#boxicon").src="assets/images/book_FILL0_wght400_GRAD0_opsz48 1.jpg";
     document.querySelector("#boxh3").innerHTML="Responsi";
-    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+    document.querySelector("#boxp").innerHTML="Responsi merupakan kelas belajar bersama yang nantinya akan diajarkan langsung oleh Mentor dari BSLC. Program ini bertujuan untuk meningkatkan pemahaman materi dan  mempersiapkan diri sebelum menghadapi UAS.";
 }
 
 function clicked7() {
@@ -100,7 +100,7 @@ function clicked7() {
     document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
     document.querySelector("#boxicon").src="assets/images/school_FILL0_wght400_GRAD0_opsz48 1.jpg";
     document.querySelector("#boxh3").innerHTML="BSLC Mengajar";
-    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+    document.querySelector("#boxp").innerHTML="BSLC Mengajar adalah kegiatan yang diselenggarakan BSLC sebagai wujud Pengabdian Pada Masyarakat, dengan target peserta merupakan anak-anak yang kurang mampu. Kegiatan ini bertujuan untuk menambah ilmu, semangat dan kreativitas bagi anak-anak yang diajarkan. Kegiatan ini juga membuka kesempatan bagi mahasiswa/i Binus University untuk meningkatkan softskill dalam mengajar.";
 }
 
 function clicked8() {
@@ -115,7 +115,7 @@ function clicked8() {
     document.querySelector("#sembilan").src="assets/images/welcoming party.jpg";
     document.querySelector("#boxicon").src="assets/images/icon-park-outline_tree.jpg";
     document.querySelector("#boxh3").innerHTML="Outing Nindya";
-    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+    document.querySelector("#boxp").innerHTML="Pengembangan Employability Nindya atau biasa disebut dengan outing nindya adalah kegiatan yang ditujukan untuk meningkatkan kualitas Nindya BSLC setelah menjalani enam bulan kepengurusan di BSLC. Selain itu Outing Nindya juga memiliki tujuan untuk meningkatkan softskill para Nindya yang selama ini telah diterapkan dalam kepengurusan dan juga mempererat kekompakan Nindya Alam Sutera dan Kemanggisan. Outing Nindya ini dikemas dengan sangat menarik dan bermanfaat oleh kakak-kakak SAC dan SADC melalui games-games yang harus dilalui para Nindya dibeberapa pos. Outing Nindya ini juga sebagai evaluasi kepengurusan yang telah berjalan selama enam bulan dan untuk membuat BSLC menjadi semakin baik.";
 }
 
 function clicked9() {
@@ -130,5 +130,5 @@ function clicked9() {
     document.querySelector("#sembilan").src="assets/images/welcoming party(1).jpg";
     document.querySelector("#boxicon").src="assets/images/tabler_ballon.jpg";
     document.querySelector("#boxh3").innerHTML="Welcoming Party";
-    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+    document.querySelector("#boxp").innerHTML="Welcome Party adalah acara tahunan BSLC untuk menyambut para mentee baru. Biasanya kegiatan Welcome Party diadakan di bulan September setelah semua batch melakukan FEP (Freshmen Enrichment Program). Dalam acara Welcome Party, para mentee diajak untuk saling mengenal satu sama lain melalui games-games yang dipersiapkan oleh para Nindya. Selain itu, para mentee juga diperkenalkan mengenai BSLC.";
 }
