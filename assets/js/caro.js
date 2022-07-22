df80099c999c1656bc3b207a2278980a367c2848
DarkMattz <febryangrady@gmail.com>
2022-07-22 15:50:34 +0700

    Update carousel

--- assets/js/caro.js
+++ assets/js/caro.js
@@ -7,6 +7,7 @@ let caller = []
 $(() => {
   $.getJSON('./assets/data/carousel.json', (data) => {
     carouselData = data
+    addDots()
     updateCarousel();
   })
 })
@@ -17,25 +18,42 @@ function updateCarousel() {
   let foreground = $('.carousel-foreground')
   let background = $('.carousel-background')
   foreground.removeClass('hide')
+
+  $(`.dot#${carouselIndex}`).addClass('selected-dot')
+
   foreground.css('background-image', `url(./assets/caro-assets/${carouselData.carousels[carouselIndex].image_name}.png)`)
   background.css('background-image', `url(./assets/caro-assets/${carouselData.carousels[(carouselIndex + 1) % carouselData.carousels.length].image_name}.png)`)
 
   if(carouselData.carousels[carouselIndex].link != null){
-    $('.carousel').on('click', () => {
+    $('.main-content').on('click', () => {
       window.open(carouselData.carousels[carouselIndex].link, '_blank')
     })
-    $('.carousel').css('cursor', 'pointer')
+    $('.main-content').css('cursor', 'pointer')
   }
+
 
   caller[1] = setTimeout(() => {
     turnOffClick()
-
     carouselIndex = (carouselIndex + 1) % carouselData.carousels.length
     foreground.addClass('hide')
     caller[2] = setTimeout(() => {
+      $(`.dot`).removeClass('selected-dot')
       updateCarousel()
     }, animationTime)
   }, carouselTime)
+}
+
+function addDots(){
+  let dotContainer = $('.dot-container')
+
+  for(let i = 0; i < carouselData.carousels.length; i++){
+    dotContainer.append(`<button class="dot" id="${i}"></button>`)
+  }
+
+  $('.dot').on('click', (e) => {
+    changeCarouselSlide(parseInt($(e.target).attr('id')))
+  })
+
 }
 
 function changeCarousel(adder){
@@ -50,17 +68,34 @@ function changeCarousel(adder){
   
   foreground.addClass('hide')
   caller[2] = setTimeout(() => {
+    $(`.dot`).removeClass('selected-dot')
     updateCarousel()
   }, animationTime)
 }
 
+function changeCarouselSlide(slide){
+  clearCaller()
+  turnOffClick()
+
+  let foreground = $('.carousel-foreground')
+  let background = $('.carousel-background')
+
+  carouselIndex = slide
+  background.css('background-image', `url(./assets/caro-assets/${carouselData.carousels[carouselIndex].image_name}.png)`)
+  
+  foreground.addClass('hide')
+  caller[2] = setTimeout(() => {
+    $(`.dot`).removeClass('selected-dot')
+    updateCarousel()
+  }, animationTime)
+}
+
+function turnOffClick(){
+  $('.main-content').off('click')
+  $('.main-content').css('cursor', 'default')
+}
 
 function clearCaller(){
   clearTimeout(caller[0])
   clearTimeout(caller[1])
-}
-
-function turnOffClick(){
-  $('.carousel').off('click')
-  $('.carousel').css('cursor', 'default')
 }
