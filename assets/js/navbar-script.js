aab5223b69e1525ca3d64cb102451c2c2e8d1962
Okky Pratama <100081121+okkyPratama@users.noreply.github.com>
2022-07-22 09:59:21 +0700

    add change navbar color onclick

--- assets/js/navbar-script.js
+++ assets/js/navbar-script.js
@@ -6,16 +6,30 @@ window.addEventListener('scroll', function () {
 })
 
 function toggleMenu() {
+    
+    changetoSticky();
+
     let menu = document.getElementById("menu")
 
-        if (menu.className === 'menu') {
-            menu.className += ' toggle'
-        } else {
-            menu.className = 'menu'; 
-        }
-
-
+    if (menu.className === 'menu') {
+        menu.className += ' toggle'
+    } else {
+        menu.className = 'menu';
     }
+
+
+}
+
+function changetoSticky() {
+    let navbar = document.getElementById("navbar")
+
+    if (navbar.className == 'navbar') {
+        navbar.className += ' sticky'
+        
+    } else {
+        navbar.className == 'navbar'
+    }
+}
 
 
 
