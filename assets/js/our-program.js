f2f1bc06d337e6b65f930025ce97ceed9ad64b06
Jerry <jerry.lucas@binus.ac.id>
2022-07-23 09:58:38 +0700

    feat: finishing our program section

---
+++ assets/js/our-program.js
@@ -0,0 +1,144 @@
+function clicked1() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-party.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK.jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition.jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/cil_birthday-cake-removebg-preview-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="Birthday Party";
+    document.querySelector("#boxp").innerHTML="Bagi kalian yang merasa ingin menambah pengetahuan atau mungkin merasa kurang paham dalam memahami materi saat berkuliah. Kalian tenang saja karena BSLC membuka kesempatan bagi mahasiswa Binusian semester 1/2 untuk bergabung sebagai mentee. Nantinya kalian sebagai mentee akan diajar oleh mentor yang merupakan kakak kelas kalian. Jadi tunggu apalagi, mari daftarkan diri kalian menjadi mentee BSLC.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/1.png";
+}
+
+function clicked2() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK(1).jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition.jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/emoji_objects_FILL0_wght400_GRAD0_opsz48_1-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="LDK CP";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/2.png";
+}
+
+function clicked3() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK.jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party(1).jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition.jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/birthdayIcon-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="Farewell Party";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/3.png";
+}
+
+function clicked4() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK.jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class(1).jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition.jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/la_chalkboard-teacher-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="BSLC Class";
+    document.querySelector("#boxp").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/4.png";
+
+}
+
+function clicked5() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK.jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition(1).jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/iconoir_leaderboard-star-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="BSLC Competition";
+    document.querySelector("#boxp").innerHTML="BSLC Competition adalah event yang bekerjasama dengan pihak sponsor untuk menciptakan kompetisi yang menarik bagi seluruh mahasiswa/i Indonesia. Kegiatan ini terdiri dari beberapa tahap penyisihan untuk menuju ke tahap final. BSLC Competition diharapkan mampu menjadi event yang menambah wawasan dan pengalaman mahasiswa/i dengan mengikuti kompetisi yang diadakan pada tahun tersebut.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/5.png";
+}
+
+function clicked6() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK.jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition.jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi(1).jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/book_FILL0_wght400_GRAD0_opsz48_1-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="Responsi";
+    document.querySelector("#boxp").innerHTML="Responsi merupakan kelas belajar bersama yang nantinya akan diajarkan langsung oleh Mentor dari BSLC. Program ini bertujuan untuk meningkatkan pemahaman materi dan  mempersiapkan diri sebelum menghadapi UAS.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/6.png";
+}
+
+function clicked7() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK.jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition.jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar(1).jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/school_FILL0_wght400_GRAD0_opsz48_1-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="BSLC Mengajar";
+    document.querySelector("#boxp").innerHTML="BSLC Mengajar adalah kegiatan yang diselenggarakan BSLC sebagai wujud Pengabdian Pada Masyarakat, dengan target peserta merupakan anak-anak yang kurang mampu. Kegiatan ini bertujuan untuk menambah ilmu, semangat dan kreativitas bagi anak-anak yang diajarkan. Kegiatan ini juga membuka kesempatan bagi mahasiswa/i Binus University untuk meningkatkan softskill dalam mengajar.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/7.png";
+}
+
+function clicked8() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK.jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition.jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya(1).jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party.jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/icon-park-outline_tree-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="Outing Nindya";
+    document.querySelector("#boxp").innerHTML="Pengembangan Employability Nindya atau biasa disebut dengan outing nindya adalah kegiatan yang ditujukan untuk meningkatkan kualitas Nindya BSLC setelah menjalani enam bulan kepengurusan di BSLC. Selain itu Outing Nindya juga memiliki tujuan untuk meningkatkan softskill para Nindya yang selama ini telah diterapkan dalam kepengurusan dan juga mempererat kekompakan Nindya Alam Sutera dan Kemanggisan. Outing Nindya ini dikemas dengan sangat menarik dan bermanfaat oleh kakak-kakak SAC dan SADC melalui games-games yang harus dilalui para Nindya dibeberapa pos. Outing Nindya ini juga sebagai evaluasi kepengurusan yang telah berjalan selama enam bulan dan untuk membuat BSLC menjadi semakin baik.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/8.png";
+}
+
+function clicked9() {
+    document.querySelector("#satu").src="assets/our-program-assets/Our Program/birthday-partybefore.jpg";
+    document.querySelector("#dua").src="assets/our-program-assets/Our Program/LDK.jpg";
+    document.querySelector("#tiga").src="assets/our-program-assets/Our Program/farewell-party.jpg";
+    document.querySelector("#empat").src="assets/our-program-assets/Our Program/bslc-class.jpg";
+    document.querySelector("#lima").src="assets/our-program-assets/Our Program/competition.jpg";
+    document.querySelector("#enam").src="assets/our-program-assets/Our Program/responsi.jpg";
+    document.querySelector("#tujuh").src="assets/our-program-assets/Our Program/mengajar.jpg";
+    document.querySelector("#delapan").src="assets/our-program-assets/Our Program/outing nindya.jpg";
+    document.querySelector("#sembilan").src="assets/our-program-assets/Our Program/welcoming party(1).jpg";
+    document.querySelector("#boxicon").src="assets/our-program-assets/Our Program/tabler_ballon-removebg-preview.png";
+    document.querySelector("#boxh3").innerHTML="Welcoming Party";
+    document.querySelector("#boxp").innerHTML="Welcome Party adalah acara tahunan BSLC untuk menyambut para mentee baru. Biasanya kegiatan Welcome Party diadakan di bulan September setelah semua batch melakukan FEP (Freshmen Enrichment Program). Dalam acara Welcome Party, para mentee diajak untuk saling mengenal satu sama lain melalui games-games yang dipersiapkan oleh para Nindya. Selain itu, para mentee juga diperkenalkan mengenai BSLC.";
+    document.querySelector("#img-content").src="assets/our-program-assets/Programs/9.png";
+}
